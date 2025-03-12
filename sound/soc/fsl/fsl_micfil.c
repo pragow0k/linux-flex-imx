da3b08f4cd6100669733da3a6a5a0208f39b5983
Prajwal <prajwal.gowda-hm@siemens.com>
2025-03-12 21:42:00 +0530

    Revert "ASoC: fsl_micfil: use GENMASK to define register bit fields"
    This reverts commit 7cd28f5fdbbf3c87a2ec497c4dc237ef0da3ea72.

--- sound/soc/fsl/fsl_micfil.c
+++ sound/soc/fsl/fsl_micfil.c
@@ -2,7 +2,6 @@
 // Copyright 2018 NXP
 
 #include <linux/atomic.h>
-#include <linux/bitfield.h>
 #include <linux/clk.h>
 #include <linux/device.h>
 #include <linux/interrupt.h>
@@ -784,22 +783,23 @@ static inline int get_pdm_clk(struct fsl_micfil *micfil,
 	int bclk;
 
 	regmap_read(micfil->regmap, REG_MICFIL_CTRL2, &ctrl2_reg);
-	osr = 16 - FIELD_GET(MICFIL_CTRL2_CICOSR, ctrl2_reg);
-	qsel = FIELD_GET(MICFIL_CTRL2_QSEL, ctrl2_reg);
+	osr = 16 - ((ctrl2_reg & MICFIL_CTRL2_CICOSR_MASK)
+		    >> MICFIL_CTRL2_CICOSR_SHIFT);
+	qsel = ctrl2_reg & MICFIL_CTRL2_QSEL_MASK;
 
 	switch (qsel) {
-	case MICFIL_QSEL_HIGH_QUALITY:
+	case MICFIL_HIGH_QUALITY:
 		bclk = rate * 8 * osr / 2; /* kfactor = 0.5 */
 		break;
-	case MICFIL_QSEL_MEDIUM_QUALITY:
-	case MICFIL_QSEL_VLOW0_QUALITY:
+	case MICFIL_MEDIUM_QUALITY:
+	case MICFIL_VLOW0_QUALITY:
 		bclk = rate * 4 * osr * 1; /* kfactor = 1 */
 		break;
-	case MICFIL_QSEL_LOW_QUALITY:
-	case MICFIL_QSEL_VLOW1_QUALITY:
+	case MICFIL_LOW_QUALITY:
+	case MICFIL_VLOW1_QUALITY:
 		bclk = rate * 2 * osr * 2; /* kfactor = 2 */
 		break;
-	case MICFIL_QSEL_VLOW2_QUALITY:
+	case MICFIL_VLOW2_QUALITY:
 		bclk = rate * osr * 4; /* kfactor = 4 */
 		break;
 	default:
@@ -967,7 +967,7 @@ static int __maybe_unused init_zcd(struct device *dev)
 	if (micfil->vad_zcd_auto) {
 		/* Zero-Crossing Detector Adjustment */
 		ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_ZCD,
-					 MICFIL_VAD0_ZCD_ZCDADJ_SHIFT,
+					 MICFIL_VAD0_ZCD_ZCDADJ_MASK,
 					 micfil->vad_zcd_adj);
 		if (ret) {
 			dev_err(dev,
@@ -979,7 +979,7 @@ static int __maybe_unused init_zcd(struct device *dev)
 
 	/* Zero-Crossing Detector Threshold */
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_ZCD,
-				 MICFIL_VAD0_ZCD_ZCDTH,
+				 MICFIL_VAD0_ZCD_ZCDTH_MASK,
 				 MICFIL_VAD0_ZCD_ZCDTH(micfil->vad_zcd_th));
 	if (ret) {
 		dev_err(dev, "Failed to set ZCDTH in ZCD_VAD0 [%d]\n", ret);
@@ -1271,7 +1271,7 @@ static int __maybe_unused init_hwvad(struct device *dev)
 
 	/* configure CIC OSR in VADCICOSR */
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_CTRL1,
-				 MICFIL_VAD0_CTRL1_CICOSR_SHIFT,
+				 MICFIL_VAD0_CTRL1_CICOSR_MASK,
 				 MICFIL_CTRL2_OSR_DEFAULT);
 	if (ret) {
 		dev_err(dev, "Failed to set CICOSR in CTRL1_VAD0i [%d]\n", ret);
@@ -1279,9 +1279,9 @@ static int __maybe_unused init_hwvad(struct device *dev)
 	}
 
 	/* configure source channel in VADCHSEL */
-	reg_val = MICFIL_VAD0_CTRL1_CHSEL_SHIFT(micfil->vad_channel);
+	reg_val = MICFIL_VAD0_CTRL1_CHSEL(micfil->vad_channel);
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_CTRL1,
-				 MICFIL_VAD0_CTRL1_CHSEL_SHIFT,
+				 MICFIL_VAD0_CTRL1_CHSEL_MASK,
 				 reg_val);
 	if (ret) {
 		dev_err(dev, "Failed to set CHSEL in CTRL1_VAD0 [%d]\n", ret);
@@ -1291,7 +1291,7 @@ static int __maybe_unused init_hwvad(struct device *dev)
 	/* configure detector frame time VADFRAMET */
 	reg_val = MICFIL_VAD0_CTRL2_FRAMET(micfil->vad_frame_time);
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_CTRL2,
-				 MICFIL_VAD0_CTRL2_FRAMET,
+				 MICFIL_VAD0_CTRL2_FRAMET_MASK,
 				 reg_val);
 	if (ret) {
 		dev_err(dev, "Failed to set FRAMET in CTRL2_VAD0 [%d]\n", ret);
@@ -1299,9 +1299,9 @@ static int __maybe_unused init_hwvad(struct device *dev)
 	}
 
 	/* configure initialization time in VADINITT */
-	reg_val = MICFIL_VAD0_CTRL1_INITT_SHIFT(micfil->vad_init_time);
+	reg_val = MICFIL_VAD0_CTRL1_INITT(micfil->vad_init_time);
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_CTRL1,
-				 MICFIL_VAD0_CTRL1_INITT_SHIFT,
+				 MICFIL_VAD0_CTRL1_INITT_MASK,
 				 reg_val);
 	if (ret) {
 		dev_err(dev, "Failed to set INITT in CTRL1_VAD0 [%d]\n", ret);
@@ -1311,7 +1311,7 @@ static int __maybe_unused init_hwvad(struct device *dev)
 	/* configure input gain in VADINPGAIN */
 	reg_val = MICFIL_VAD0_CTRL2_INPGAIN(micfil->vad_input_gain);
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_CTRL2,
-				 MICFIL_VAD0_CTRL2_INPGAIN,
+				 MICFIL_VAD0_CTRL2_INPGAIN_MASK,
 				 reg_val);
 	if (ret) {
 		dev_err(dev, "Failed to set INPGAIN in CTRL2_VAD0 [%d]\n", ret);
@@ -1321,7 +1321,7 @@ static int __maybe_unused init_hwvad(struct device *dev)
 	/* configure sound gain in SGAIN */
 	reg_val = MICFIL_VAD0_SCONFIG_SGAIN(micfil->vad_sound_gain);
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_SCONFIG,
-				 MICFIL_VAD0_SCONFIG_SGAIN,
+				 MICFIL_VAD0_SCONFIG_SGAIN_MASK,
 				 reg_val);
 	if (ret) {
 		dev_err(dev, "Failed to set SGAIN in SCONFIG_VAD0 [%d]\n", ret);
@@ -1331,7 +1331,7 @@ static int __maybe_unused init_hwvad(struct device *dev)
 	/* configure noise gain in NGAIN */
 	reg_val = MICFIL_VAD0_NCONFIG_NGAIN(micfil->vad_noise_gain);
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_NCONFIG,
-				 MICFIL_VAD0_NCONFIG_NGAIN,
+				 MICFIL_VAD0_NCONFIG_NGAIN_MASK,
 				 reg_val);
 	if (ret) {
 		dev_err(dev, "Failed to set NGAIN in NCONFIG_VAD0 [%d]\n", ret);
@@ -1341,7 +1341,7 @@ static int __maybe_unused init_hwvad(struct device *dev)
 	/* configure or clear the VADNFILADJ based on mode */
 	reg_val = MICFIL_VAD0_NCONFIG_NFILADJ(micfil->vad_nfil_adjust);
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_NCONFIG,
-				 MICFIL_VAD0_NCONFIG_NFILADJ,
+				 MICFIL_VAD0_NCONFIG_NFILADJ_MASK,
 				 reg_val);
 	if (ret) {
 		dev_err(dev,
@@ -1353,7 +1353,7 @@ static int __maybe_unused init_hwvad(struct device *dev)
 	/* enable the high-pass filter in VADHPF */
 	reg_val = MICFIL_VAD0_CTRL2_HPF(micfil->vad_hpf);
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_VAD0_CTRL2,
-				 MICFIL_VAD0_CTRL2_HPF,
+				 MICFIL_VAD0_CTRL2_HPF_MASK,
 				 reg_val);
 	if (ret) {
 		dev_err(dev, "Failed to set HPF in CTRL2_VAD0 [%d]\n", ret);
@@ -1520,8 +1520,8 @@ static int fsl_micfil_trigger(struct snd_pcm_substream *substream, int cmd,
 		 * 11 - reserved
 		 */
 		ret = regmap_update_bits(micfil->regmap, REG_MICFIL_CTRL1,
-				MICFIL_CTRL1_DISEL,
-				FIELD_PREP(MICFIL_CTRL1_DISEL, MICFIL_CTRL1_DISEL_DMA));
+					 MICFIL_CTRL1_DISEL_MASK,
+					 (1 << MICFIL_CTRL1_DISEL_SHIFT));
 		if (ret) {
 			dev_err(dev, "failed to update DISEL bits\n");
 			return ret;
@@ -1550,8 +1550,8 @@ static int fsl_micfil_trigger(struct snd_pcm_substream *substream, int cmd,
 		}
 
 		ret = regmap_update_bits(micfil->regmap, REG_MICFIL_CTRL1,
-				MICFIL_CTRL1_DISEL,
-				FIELD_PREP(MICFIL_CTRL1_DISEL, MICFIL_CTRL1_DISEL_DISABLE));
+					 MICFIL_CTRL1_DISEL_MASK,
+					 (0 << MICFIL_CTRL1_DISEL_SHIFT));
 		if (ret) {
 			dev_err(dev, "failed to update DISEL bits\n");
 			return ret;
@@ -1576,8 +1576,8 @@ static int fsl_set_clock_params(struct device *dev, unsigned int rate)
 
 	/* set CICOSR */
 	ret |= regmap_update_bits(micfil->regmap, REG_MICFIL_CTRL2,
-				 MICFIL_CTRL2_CICOSR,
-				 FIELD_PREP(MICFIL_CTRL2_CICOSR, MICFIL_CTRL2_CICOSR_DEFAULT));
+				 MICFIL_CTRL2_CICOSR_MASK,
+				 MICFIL_CTRL2_OSR_DEFAULT);
 	if (ret)
 		dev_err(dev, "failed to set CICOSR in reg 0x%X\n",
 			REG_MICFIL_CTRL2);
@@ -1588,8 +1588,7 @@ static int fsl_set_clock_params(struct device *dev, unsigned int rate)
 		ret = -EINVAL;
 
 	ret |= regmap_update_bits(micfil->regmap, REG_MICFIL_CTRL2,
-				 MICFIL_CTRL2_CLKDIV,
-				 FIELD_PREP(MICFIL_CTRL2_CLKDIV, clk_div));
+				 MICFIL_CTRL2_CLKDIV_MASK, clk_div);
 	if (ret)
 		dev_err(dev, "failed to set CLKDIV in reg 0x%X\n",
 			REG_MICFIL_CTRL2);
@@ -1733,8 +1732,7 @@ static int fsl_micfil_dai_probe(struct snd_soc_dai *cpu_dai)
 
 	/* set qsel to medium */
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_CTRL2,
-				 MICFIL_CTRL2_QSEL,
-				 FIELD_PREP(MICFIL_CTRL2_QSEL, MICFIL_QSEL_VLOW0_QUALITY));
+				 MICFIL_CTRL2_QSEL_MASK, MICFIL_VLOW0_QUALITY);
 	if (ret) {
 		dev_err(dev, "failed to set quality mode bits, reg 0x%X\n",
 			REG_MICFIL_CTRL2);
@@ -1763,9 +1761,10 @@ static int fsl_micfil_dai_probe(struct snd_soc_dai *cpu_dai)
 				  &micfil->dma_params_rx);
 
 	/* FIFO Watermark Control - FIFOWMK*/
+	val = MICFIL_FIFO_CTRL_FIFOWMK(micfil->soc->fifo_depth) - 1;
 	ret = regmap_update_bits(micfil->regmap, REG_MICFIL_FIFO_CTRL,
-			MICFIL_FIFO_CTRL_FIFOWMK,
-			FIELD_PREP(MICFIL_FIFO_CTRL_FIFOWMK, micfil->soc->fifo_depth - 1));
+				 MICFIL_FIFO_CTRL_FIFOWMK_MASK,
+				 val);
 	if (ret) {
 		dev_err(dev, "failed to set FIFOWMK\n");
 		return ret;
@@ -1991,11 +1990,11 @@ static irqreturn_t micfil_isr(int irq, void *devid)
 	regmap_read(micfil->regmap, REG_MICFIL_CTRL1, &ctrl1_reg);
 	regmap_read(micfil->regmap, REG_MICFIL_FIFO_STAT, &fifo_stat_reg);
 
-	dma_enabled = FIELD_GET(MICFIL_CTRL1_DISEL, ctrl1_reg) == MICFIL_CTRL1_DISEL_DMA;
+	dma_enabled = MICFIL_DMA_ENABLED(ctrl1_reg);
 
 	/* Channel 0-7 Output Data Flags */
 	for (i = 0; i < MICFIL_OUTPUT_CHANNELS; i++) {
-		if (stat_reg & MICFIL_STAT_CHXF(i))
+		if (stat_reg & MICFIL_STAT_CHXF_MASK(i))
 			dev_dbg(&pdev->dev,
 				"Data available in Data Channel %d\n", i);
 		/* if DMA is not enabled, field must be written with 1
@@ -2004,17 +2003,17 @@ static irqreturn_t micfil_isr(int irq, void *devid)
 		if (!dma_enabled)
 			regmap_write_bits(micfil->regmap,
 					  REG_MICFIL_STAT,
-					  MICFIL_STAT_CHXF(i),
+					  MICFIL_STAT_CHXF_MASK(i),
 					  1);
 	}
 
 	for (i = 0; i < MICFIL_FIFO_NUM; i++) {
-		if (fifo_stat_reg & MICFIL_FIFO_STAT_FIFOX_OVER(i))
+		if (fifo_stat_reg & MICFIL_FIFO_STAT_FIFOX_OVER_MASK(i))
 			dev_dbg(&pdev->dev,
 				"FIFO Overflow Exception flag for channel %d\n",
 				i);
 
-		if (fifo_stat_reg & MICFIL_FIFO_STAT_FIFOX_UNDER(i))
+		if (fifo_stat_reg & MICFIL_FIFO_STAT_FIFOX_UNDER_MASK(i))
 			dev_dbg(&pdev->dev,
 				"FIFO Underflow Exception flag for channel %d\n",
 				i);
